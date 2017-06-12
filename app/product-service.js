5fad666beb40d402913ea2102550b1399cb8f5e6
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-12 10:07:03 +0100

    added product servoce

---
+++ app/product-service.js
@@ -0,0 +1,55 @@
+const cote = require('cote')
+
+const productResponder = new cote.Responder({
+    name: 'SAS-PRODUCT-SERVICE: product-responder ',
+    namespace: '',
+    respondsTo: ['product.create', 'product.update', 'product.get', 'product.delete']
+})
+
+/**
+ * Responder: {model, body}
+ * Create a product. req.body eg {name: 'foo'} is the object to be created
+ */
+productResponder.on('product.create', (req) => {
+    switch (req.model) {
+        case "product": return Store.create(req.body)
+
+        default: return null
+    }
+})
+
+/**
+ * Responder: {model, body}
+ * Update a product. req.query is an object eg {id: 1}, req.body eg {name: 'foo'} is the object to be updated
+ */
+productResponder.on('product.update', (req) => {
+    switch (req.model) {
+        case "product": return Store.update(req.query, req.body)
+
+        default: return null
+    }
+})
+
+/**
+ * Responder: {model, query}
+ * Gets a product. req.query is an object eg {id: 1}
+ */
+productResponder.on('product.get', (req) => {
+    switch (req.model) {
+        case "product": return Store.get(req.query)
+
+        default: return null
+    }
+})
+
+/**
+ * Responder: {model, query}
+ * Deletes a product. req.query is an object eg {id: 1}
+ */
+productResponder.on('product.delete', (req) => {
+    switch (req.model) {
+        case "product": return Store.delete(req.query)
+
+        default: return null
+    }
+})
